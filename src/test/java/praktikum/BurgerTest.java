f0e016d6bed517bce44016ae7fbae46dfd2e37eb
Roman Krivorukov <romankrivorukov@yandex.ru>
2023-01-14 16:44:05 +0300

    добавил отчет jacoco, параметризация

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -1,13 +1,25 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
 
 import java.util.StringJoiner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(Parameterized.class)
 public class BurgerTest {
+
+    private final Burger burger;
+    private final float price;
+
+    public BurgerTest(Burger burger, float price) {
+        this.burger = burger;
+        this.price = price;
+    }
 
     @Test
     public void setBuns() {
@@ -58,15 +70,7 @@ public class BurgerTest {
 
     @Test
     public void getPrice() {
-        Burger burger = new Burger();
-        burger.setBuns(new Bun("bun1", 5.0f));
-        Ingredient i1 = new Ingredient(IngredientType.SAUCE, "test1", 10.0f);
-        Ingredient i2 = new Ingredient(IngredientType.FILLING, "test2", 15.0f);
-        Ingredient i3 = new Ingredient(IngredientType.SAUCE, "test3", 20.0f);
-        burger.addIngredient(i1);
-        burger.addIngredient(i2);
-        burger.addIngredient(i3);
-        assertEquals(55.0f, burger.getPrice(), 0.0001);
+        assertEquals(price, burger.getPrice(), 0.0001);
     }
 
     @Test
@@ -91,5 +95,28 @@ public class BurgerTest {
                 .add("");
         assertEquals(joiner.toString(), burger.getReceipt());
 
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        Bun bun = Mockito.mock(Bun.class);
+        Mockito.doReturn(5.0f).when(bun).getPrice();
+        Burger burger1 = new Burger();
+        burger1.setBuns(bun);
+        Ingredient i1 = new Ingredient(IngredientType.SAUCE, "test1", 10.0f);
+        Ingredient i2 = new Ingredient(IngredientType.FILLING, "test2", 15.0f);
+        Ingredient i3 = new Ingredient(IngredientType.SAUCE, "test3", 20.0f);
+        burger1.addIngredient(i1);
+        burger1.addIngredient(i2);
+        burger1.addIngredient(i3);
+
+        Burger burger2 = new Burger();
+        burger2.setBuns(bun);
+        Ingredient i4 = new Ingredient(IngredientType.FILLING, "test4", 10.0f);
+        burger2.addIngredient(i4);
+        return new Object[][] {
+                {burger1, 55.0f},
+                {burger2, 20.0f}
+        };
     }
 }
